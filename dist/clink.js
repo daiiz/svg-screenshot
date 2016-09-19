e67eb8776666154d7e831be489c41a644d4b7a00
daiiz <da.iz.mg@gmail.com>
2016-09-19 18:10:27 +0900

    add Gyazo, GyazoSearch

---
+++ dist/clink.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * https://svgscreenshot.appspot.com/c/<ScreenShot-id> を開く
+ */
+
+var CLink = function () {
+    function CLink() {
+        var tab = arguments.length <= 0 || arguments[0] === undefined ? null : arguments[0];
+
+        _classCallCheck(this, CLink);
+
+        this.tab = tab;
+        this.cn = 'daiiz-svgss-btn';
+    }
+
+    _createClass(CLink, [{
+        key: 'Gyazo',
+
+
+        /**
+         * サービス別に処理を定義する
+         */
+        value: function Gyazo() {
+            $('body').on('mouseenter', '.metadata-row', function (e) {
+                var $t = $(e.target).closest('.metadata-row');
+                if ($t.find('.metadata-key > i').hasClass('gy-icon-title')) {
+                    var $v = $t.find('.metadata-value');
+                    if ($v.find('a.daiiz-svgss-btn')) {
+                        var fileName = $v[0].innerHTML;
+                        var screenShotId = CLink.extractScreenShotId(fileName);
+                        if (CLink.checkScreenShotId(screenShotId)) {
+                            var $a = CLink.baseATag(fileName);
+                            $a.attr('href', CLink.getCLink(screenShotId));
+                            $v[0].innerHTML = $a[0].outerHTML;
+                        }
+                    }
+                }
+            });
+        }
+    }, {
+        key: 'GyazoSearch',
+        value: function GyazoSearch() {
+            var extract = function extract($triggerElement) {
+                var $thumbInner = $triggerElement.closest('.thumb-inner');
+                var fileName = $thumbInner.find('.title')[0].innerHTML;
+                var screenShotId = CLink.extractScreenShotId(fileName);
+                return CLink.checkScreenShotId(screenShotId);
+            };
+
+            var tipStyle = 'position: absolute; top: -24px; left: 2px; background: #555; color: #fff; text-align: left; ' + 'z-index: 1000; padding: 4px; line-height: 1; border-radius: 3px; cursor: pointer;';
+            var $tip = $('<div style="' + tipStyle + '">Open ScreenShot</div>');
+
+            var aStyle = 'text-decoration: none; display: inline; width: initial; height: initial; position: static;';
+            var $a = $('<a style="' + aStyle + '" target="_blank" href="#">');
+            $a.append($tip);
+
+            CLink.showMenu('.search-thumb', '.thumb-inner', $a, extract);
+        }
+    }], [{
+        key: 'targets',
+        value: function targets() {
+            var matchUrls = [['GyazoSearch', 'https://gyazo.com/search'], ['Gyazo', 'https://gyazo.com/(.+)'], ['GooglePhoto', 'https://photos.google.com/photo/(.+)']];
+            return matchUrls;
+        }
+    }, {
+        key: 'matchUrl',
+        value: function matchUrl(pageUrl) {
+            var targets = CLink.targets();
+            for (var i = 0; i < targets.length; i++) {
+                var t = targets[i][1];
+                var r = new RegExp(t, 'i');
+                if (pageUrl.match(r)) {
+                    return targets[i][0];
+                }
+            }
+            return false;
+        }
+
+        // 右クリックメニュー生成
+
+    }, {
+        key: 'showMenu',
+        value: function showMenu(trigger, insertClosestBefore, $tooltip, extractFunction) {
+            var $body = $('body');
+            $body.on('mouseenter', trigger, function (e) {
+                $('.daiiz-svgss-btn').remove();
+
+                var $t = $(e.target);
+                var screenShotId = extractFunction($t);
+                if (screenShotId) {
+                    $tooltip.addClass("daiiz-svgss-btn");
+                    $tooltip.attr('href', CLink.getCLink(screenShotId));
+                    $t.closest(insertClosestBefore).before($tooltip);
+                }
+            });
+        }
+    }, {
+        key: 'extractScreenShotId',
+        value: function extractScreenShotId(str) {
+            var res = '';
+            res = (str.split('-')[1] || '').split('.')[0];
+            res = res.split('(')[0];
+            return res;
+        }
+    }, {
+        key: 'checkScreenShotId',
+        value: function checkScreenShotId(sid) {
+            if (sid.length > 5 && +sid % 1 === 0) return sid;
+            return false;
+        }
+    }, {
+        key: 'getCLink',
+        value: function getCLink() {
+            var screenShotId = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+
+            //var screenShotId = CLink.extractScreenShotId(idStr);
+            var base = 'https://svgscreenshot.appspot.com/c';
+            return base + '/' + screenShotId;
+        }
+    }, {
+        key: 'baseATag',
+        value: function baseATag(a) {
+            return $('<a title="SVGスクリーンショットを開く" class="daiiz-svgss-btn" target="_blank" href="#" style="cursor: pointer">' + a + '</a>');
+        }
+    }]);
+
+    return CLink;
+}();
+
+var cc = new CLink();
---
+++ src/clink.es6
@@ -0,0 +1,110 @@
+/**
+ * https://svgscreenshot.appspot.com/c/<ScreenShot-id> を開く
+ */
+class CLink {
+    constructor (tab=null) {
+        this.tab = tab;
+        this.cn = 'daiiz-svgss-btn';
+    }
+
+    static targets () {
+        var matchUrls = [
+            ['GyazoSearch', 'https://gyazo.com/search'],
+            ['Gyazo', 'https://gyazo.com/(.+)'],
+            ['GooglePhoto', 'https://photos.google.com/photo/(.+)']
+        ];
+        return matchUrls;
+    }
+
+    static matchUrl (pageUrl) {
+        var targets = CLink.targets();
+        for (var i = 0; i < targets.length; i++) {
+            var t = targets[i][1];
+            var r = new RegExp(t, 'i');
+            if (pageUrl.match(r)) {
+                return targets[i][0];
+            }
+        }
+        return false;
+    }
+
+    // 右クリックメニュー生成
+    static showMenu (trigger, insertClosestBefore, $tooltip, extractFunction) {
+        var $body = $('body');
+        $body.on('mouseenter', trigger, e => {
+            $('.daiiz-svgss-btn').remove();
+
+            var $t = $(e.target);
+            var screenShotId = extractFunction($t);
+            if (screenShotId) {
+                $tooltip.addClass("daiiz-svgss-btn");
+                $tooltip.attr('href', CLink.getCLink(screenShotId));
+                $t.closest(insertClosestBefore).before($tooltip);
+            }
+        });
+    }
+
+    static extractScreenShotId (str) {
+        var res = '';
+        res = (str.split('-')[1] || '').split('.')[0];
+        res = res.split('(')[0];
+        return res;
+    }
+
+    static checkScreenShotId (sid) {
+        if (sid.length > 5 && +sid % 1 === 0) return sid;
+        return false;
+    }
+
+    static getCLink (screenShotId='') {
+        //var screenShotId = CLink.extractScreenShotId(idStr);
+        var base = 'https://svgscreenshot.appspot.com/c';
+        return `${base}/${screenShotId}`;
+    }
+
+    static baseATag (a) {
+        return $('<a title="SVGスクリーンショットを開く" class="daiiz-svgss-btn" target="_blank" href="#" style="cursor: pointer">'+ a +'</a>');
+    }
+
+    /**
+     * サービス別に処理を定義する
+     */
+    Gyazo () {
+        $('body').on('mouseenter', '.metadata-row', e => {
+            var $t = $(e.target).closest('.metadata-row');
+            if ($t.find('.metadata-key > i').hasClass('gy-icon-title')) {
+                var $v = $t.find('.metadata-value');
+                if ($v.find('a.daiiz-svgss-btn')) {
+                    var fileName = $v[0].innerHTML;
+                    var screenShotId = CLink.extractScreenShotId(fileName);
+                    if (CLink.checkScreenShotId(screenShotId)) {
+                        var $a = CLink.baseATag(fileName);
+                        $a.attr('href', CLink.getCLink(screenShotId));
+                        $v[0].innerHTML = $a[0].outerHTML;
+                    }
+                }
+            }
+        });
+    }
+
+    GyazoSearch () {
+        var extract = ($triggerElement) => {
+            var $thumbInner = $triggerElement.closest('.thumb-inner');
+            var fileName = $thumbInner.find('.title')[0].innerHTML;
+            var screenShotId = CLink.extractScreenShotId(fileName);
+            return CLink.checkScreenShotId(screenShotId);
+        };
+
+        var tipStyle = 'position: absolute; top: -24px; left: 2px; background: #555; color: #fff; text-align: left; ' +
+            'z-index: 1000; padding: 4px; line-height: 1; border-radius: 3px; cursor: pointer;';
+        var $tip = $(`<div style="${tipStyle}">Open ScreenShot</div>`);
+
+        var aStyle = 'text-decoration: none; display: inline; width: initial; height: initial; position: static;';
+        var $a = $(`<a style="${aStyle}" target="_blank" href="#">`);
+        $a.append($tip);
+
+        CLink.showMenu('.search-thumb', '.thumb-inner', $a, extract);
+    }
+}
+
+var cc = new CLink();
